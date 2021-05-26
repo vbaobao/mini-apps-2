372af32a7cc8088b7ac604e006c3ee3fb89625e5
vbaobao <vbao.main@gmail.com>
2021-05-26 00:52:55 -0700

    create base click hanlder and pagination

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,9 +1,36 @@
-import React, { useState }from 'react';
+import axios from 'axios';
+import React, { useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
+import SearchBar from './SearchBar.jsx';
+import SearchResults from './SearchResults.jsx';
 
 function App(props) {
-  const [data, setData] = useState(null);
-  return (<h1>Test</h1>);
+  const [data, setData] = useState([]);
+
+  const handlePageChange = useCallback((e) => {
+    console.log(e.selected);
+  }, []);
+
+  const handleSubmit = useCallback((e) => {
+    console.log(e);
+  }, []);
+
+  return (
+    <div>
+      Test
+      <SearchBar handleSubmit={handleSubmit} />
+      <SearchResults data={data}/>
+      <ReactPaginate
+          pageCount={Math.ceil(data.length/10)}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={2}
+          previousLabel={'<<'}
+          nextLabel={'>>'}
+          onPageChange={handlePageChange}
+        />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
