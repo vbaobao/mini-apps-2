b0a57510482933c45cf0101cb1c378f748433f29
vbaobao <vbao.main@gmail.com>
2021-05-25 23:57:43 -0700

    create base React component

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,9 @@
+import React, { useState }from 'react';
+import ReactDOM from 'react-dom';
 
+function App(props) {
+  const [data, setData] = useState(null);
+  return (<h1>Test</h1>);
+}
 
+ReactDOM.render(<App />, document.getElementById('root'));
