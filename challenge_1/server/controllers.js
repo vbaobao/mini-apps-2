d97a5343020e558161c9af15e67432a4a9a0149d
vbaobao <vbao.main@gmail.com>
2021-05-25 23:58:27 -0700

    write functional search controller

--- challenge_1/server/controllers.js
+++ challenge_1/server/controllers.js
@@ -1,3 +1,17 @@
 require('../server/database.js');
 const mongoose = require('mongoose');
-const Event = require('./server/schema.js');
+const Event = require('../server/schema.js');
+
+const Search = async (req, res) => {
+  const searchTerm = req.body.query;
+  try {
+    const response = await Event.find({ $text: { $search: searchTerm } });
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+module.exports = {
+  Search,
+};
