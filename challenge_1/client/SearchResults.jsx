d96b874a7aad338b4921c6a5a0987921df9efd06
vbaobao <vbao.main@gmail.com>
2021-05-27 02:17:48 -0700

    move location of tags

--- challenge_1/client/SearchResults.jsx
+++ challenge_1/client/SearchResults.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
 function SearchResults (props) {
-  console.log(props.data);
   const results = props.data.map((event) => {
+    const lang = event.lang
+      ? <div className=""><span className="lang">Language</span> <div dangerouslySetInnerHTML={{__html: event.lang}}></div></div>
+      : '';
+    const category1 = event.category1
+      ? <div><span className="category1">Category 1</span> <div dangerouslySetInnerHTML={{__html: event.category1}}></div></div>
+      : '';
+    const category2 = event.category2
+      ? <div><span className="category2">Category 2</span> <div dangerouslySetInnerHTML={{__html: event.category2}}></div></div>
+      : '';
+    const granularity = event.granularity
+      ? <div><span className="granularity">Granularity</span> <div dangerouslySetInnerHTML={{__html: event.granularity}}></div></div>
+      : '';
     return (
-      <div key={event._id}>
-        <div>
-          <div><span className="date">{event.date}</span>{event.description}</div>
+      <div key={event._id} className="results">
+        <div className="results_tags">
+          <div>
+            <span className="date">Date</span> <div>{event.date}</div>
+          </div>
+          {lang} {category1} {category2} {granularity}
         </div>
-        <div>
-          <span className="lang">{event.lang}</span>
-          <span className="category1">{event.category1}</span>
-          <span className="category2">{event.category2}</span>
-          <span className="granularity">{event.granularity}</span>
-        </div>
+        <p dangerouslySetInnerHTML={{__html: event.description}}></p>
       </div>
     );
   });
