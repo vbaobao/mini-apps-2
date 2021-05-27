112a4b4ca1c7aecb3b4ab0982456ca22fba28a2f
vbaobao <vbao.main@gmail.com>
2021-05-26 19:43:17 -0700

    create results components

--- challenge_1/client/SearchResults.jsx
+++ challenge_1/client/SearchResults.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function SearchResults (props) {
-  return (<p>{JSON.stringify(props.data)}</p>);
+  console.log(props.data);
+  const results = props.data.map((event) => {
+    return (
+      <div key={event._id}>
+        <div>
+          <div><span className="date">{event.date}</span>{event.description}</div>
+        </div>
+        <div>
+          <span className="lang">{event.lang}</span>
+          <span className="category1">{event.category1}</span>
+          <span className="category2">{event.category2}</span>
+          <span className="granularity">{event.granularity}</span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>{results}</div>
+  );
 }
 
 export default SearchResults;
