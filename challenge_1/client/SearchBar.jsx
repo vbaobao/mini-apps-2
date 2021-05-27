90b86c2f54ff36ed9fbefcab5491c15731c701df
vbaobao <vbao.main@gmail.com>
2021-05-26 18:24:55 -0700

    write working change and submit handlers

--- challenge_1/client/SearchBar.jsx
+++ challenge_1/client/SearchBar.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar (props) {
-  return (<p>Search Bar</p>);
+  const [val, setVal] = useState('');
+  
+  const handleChange = (e) => {
+    setVal(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.handleSubmit(val);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input name='searchValue' type='text' value={val} onChange={handleChange} />
+        </label>
+        <input type="submit" value="search!" />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
