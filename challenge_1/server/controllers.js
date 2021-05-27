18823dcbee8f6fb55ec9305bfe56a1dbb331e590
vbaobao <vbao.main@gmail.com>
2021-05-26 19:43:41 -0700

    refactor code for pagination

--- challenge_1/server/controllers.js
+++ challenge_1/server/controllers.js
@@ -4,9 +4,13 @@ const Event = require('../server/schema.js');
 
 const Search = async (req, res) => {
   const searchTerm = req.query.search;
+  const page = Number(req.query.page);
+  const offset = Number(req.query.offset);
   try {
     const response = await Event.find({ $text: { $search: searchTerm } });
-    res.send(response);
+    const max = Math.ceil(response.length/offset);
+    const data = response.slice((page - 1) * offset, page * offset) || response.slice((page - 1) * offset);
+    res.send({ data: data, maxPages: max });
   } catch (err) {
     res.status(400).send(err.message);
   }
