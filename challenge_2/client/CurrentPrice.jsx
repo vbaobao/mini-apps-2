2789e8b97717a401046737dced42a070f41c04bf
vbaobao <vbao.main@gmail.com>
2021-05-28 01:44:27 -0700

    build basic format

--- challenge_2/client/CurrentPrice.jsx
+++ challenge_2/client/CurrentPrice.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 function CurrentPrice (props) {
+  const crypto = props.data?.chartName ? props.data.chartName : '';
+  const rate = props.data?.bpi?.USD.rate ? props.data.bpi.USD.rate : '';
+  const currencyCode = props.data?.bpi?.USD.code ? props.data.bpi.USD.code : '';
+  const lastUpdate = props.data?.time?.updated ? props.data.time.updated : '';
   return (
     <div>
-      <h2>Current Price</h2>
-      {JSON.stringify(props.data)}
+      <div>
+        <span className="crypto_name">
+        {crypto}
+        </span>
+        <span className="crypto_price">
+          <span className="crypto_rate">{rate}</span> {currencyCode}
+        </span>
+      </div>
+      <div>
+        <b>Last updated:</b> {lastUpdate}
+      </div>
     </div>
   );
 };
