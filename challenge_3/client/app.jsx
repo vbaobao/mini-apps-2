9b31b6735192c3a2542361b08cfbaf5a946eeeb3
vbaobao <vbao.main@gmail.com>
2021-06-22 15:29:09 -0700

    split component to render and component files

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,43 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import Pins from './Pins.jsx';
-import Scoreboard from './Scoreboard.jsx';
+import Bowling from './Bowling.jsx';
 
-function App (props) {
-  const [scores, setScores] = useState([]);
-  const [isFirstFrame, setIsFirstFrame] = useState(true);
-  const [currentFrame, setCurrentFrame] = useState(1);
-  const maxFrames = 10;
-
-  const toggleFrame = () => setIsFirstFrame(!isFirstFrame);
-
-  const selectPin = (pin) => {
-    const updatedScores = [...scores];
-
-    if (isFirstFrame) {
-      updatedScores.push({ first: pin });
-    } else {
-      const lastRound = updatedScores.length - 1;
-      updatedScores[lastRound].second = pin;
-    }
-
-    setScores(updatedScores);
-
-    if (currentFrame + 1 === maxFrames) {
-      console.log('Game is over!');
-    } else {
-      setCurrentFrame(currentFrame + 1);
-      toggleFrame();
-    }
-  };
-
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <Pins selectPin={selectPin} />
-      <Scoreboard scores={scores} />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Bowling />, document.getElementById('root'));
