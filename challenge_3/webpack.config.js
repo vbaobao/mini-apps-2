b800eae9937000e83df859b487a5ae683d094d61
vbaobao <vbao.main@gmail.com>
2021-06-21 18:47:43 -0700

    debug path

--- challenge_3/webpack.config.js
+++ challenge_3/webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname + '/client/app.js'),
+  entry: path.resolve(__dirname, 'client/app.jsx'),
   output: {
-    path: path.resolve(__dirname, '/public'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
