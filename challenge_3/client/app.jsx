a141e3dd9f72fbb8a335d5db6ace7decf146d821
vbaobao <vbao.main@gmail.com>
2021-06-21 20:51:38 -0700

    write basic game logic

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import Pins from './Pins.jsx';
+import Scoreboard from './Scoreboard.jsx';
 
 function App (props) {
-  return <h1>Hello World</h1>;
+  const [scores, setScores] = useState([]);
+  const [isFirstFrame, setIsFirstFrame] = useState(true);
+  const [currentFrame, setCurrentFrame] = useState(1);
+  const maxFrames = 10;
+
+  const toggleFrame = () => setIsFirstFrame(!isFirstFrame);
+
+  const selectPin = (pin) => {
+    const updatedScores = [...scores];
+
+    if (isFirstFrame) {
+      updatedScores.push({ first: pin });
+    } else {
+      const lastRound = updatedScores.length - 1;
+      updatedScores[lastRound].second = pin;
+    }
+
+    setScores(updatedScores);
+
+    if (currentFrame + 1 === maxFrames) {
+      console.log('Game is over!');
+    } else {
+      setCurrentFrame(currentFrame + 1);
+      toggleFrame();
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Pins selectPin={selectPin} />
+      <Scoreboard scores={scores} />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
