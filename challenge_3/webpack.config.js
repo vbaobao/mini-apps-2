55aacf155ca418d7121056d61f918f2168bf3ee9
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-06-21 17:25:27 -0700

    basic webpack config

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname + '/client/app.js'),
+  output: {
+    path: path.resolve(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
