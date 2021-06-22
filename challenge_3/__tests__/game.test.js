32e2891fee1db096808239b3929a59d52a23cbcd
vbaobao <vbao.main@gmail.com>
2021-06-22 15:28:56 -0700

    add component hook testing

--- challenge_3/__tests__/game.test.js
+++ challenge_3/__tests__/game.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import sinon from 'sinon';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import Bowling from '../client/Bowling.jsx';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Name', () => {
   test('name', () => {
     expect(1).toBe(1);
+  });
+
+  test('simulates click events', () => {
+    const wrapper = mount(<Bowling />);
+    expect(JSON.stringify(wrapper.find('Scoreboard').prop('scores'))).toBe(JSON.stringify([]));
+    wrapper.find('button[value=1]').simulate('click');
+    expect(JSON.stringify(wrapper.find('Scoreboard').prop('scores'))).toBe(JSON.stringify([{ first: 1 }]));
   });
 });
