0c4f79ba28190e5e178129cda35f5dbb2aa70aee
vbaobao <vbao.main@gmail.com>
2021-06-22 17:05:51 -0700

    add prepopulated score boxes

--- challenge_3/client/Pins.jsx
+++ challenge_3/client/Pins.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 
 function Pins (props) {
   const numbers = [];
-
-  const clickHandler = (e) => {
-    props.selectPin(e.target.value);
-  };
+  const pinsRemaining = props.isFirstFrame ? 10 : 10 - props.currentRound.first;
 
   for (let i = 0; i <= 10; i++) {
     numbers.push(i);
   }
 
+  const clickHandler = (e) => {
+    props.selectPin(e.target.value);
+  };
+
   const pins = numbers.map((pin) => {
+    const button = pin <= pinsRemaining && props.gameState
+      ? <button key={`pin-button-${pin}`} value={pin} onClick={clickHandler}>{pin}</button>
+      : <button key={`pin-button-${pin}`} value={pin} className="inactive">{pin}</button>;
+
     return (
       <div key={`pin-${pin}`}>
-        <button key={`pin-button-${pin}`} value={pin} onClick={clickHandler}>
-          {pin}
-        </button>
+        {button}
       </div>
     )
   });
 
   return (
-    <div>
+    <div className="pins-wrapper">
       {pins}
     </div>
   );
