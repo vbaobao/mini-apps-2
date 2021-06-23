cf11cc9e8f359ac10199f38870a32893451d7a2e
vbaobao <vbao.main@gmail.com>
2021-06-22 17:06:05 -0700

    add prepopulated score boxes

--- challenge_3/client/Scoreboard.jsx
+++ challenge_3/client/Scoreboard.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 
 function Scoreboard (props) {
-  const frames = props.scores.map((round) => {
+  const gameRound = [];
+  for (let i = 0; i < 10; i++) {
+    gameRound.push(i);
+  }
+
+  const frames = gameRound.map((roundIndex) => {
+    const round = props.scores[roundIndex];
+    const first = (round && round.first !== null) ? round.first : ' ';
+    const second = (round && round.second !== null) ? round.second : ' ';
+    const total = (round && round.total !== null) ? round.total : ' ';
     return (
-      <div key={`round-${round}`}>
-        <div>
-          <div className="frame">{round.first}</div>
-          <div className="frame">{round.second}</div>
+      <div className="score-container" key={`round-${roundIndex}`}>
+        <div className="frame-container">
+          <div className="frame">{first}</div>
+          <div className="frame">{second}</div>
         </div>
-        <div className="total-score"></div>
+        <div className="total-score">{total}</div>
       </div>
     );
   });
 
   return (
-    <div>
+    <div className="score-wrapper">
       {frames}
     </div>
   );
