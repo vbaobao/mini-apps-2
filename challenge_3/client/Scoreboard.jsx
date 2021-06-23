55b19078ad16e977013600ab7127e3b8f3ab3ef2
vbaobao <vbao.main@gmail.com>
2021-06-22 23:04:37 -0700

    add special elements for perfect game

--- challenge_3/client/Scoreboard.jsx
+++ challenge_3/client/Scoreboard.jsx
@@ -9,13 +9,19 @@ function Scoreboard (props) {
   const frames = gameRound.map((roundIndex) => {
     const round = props.scores[roundIndex];
     const first = (round && round.first !== null) ? round.first : ' ';
-    const second = (round && round.second !== null) ? round.second : ' ';
+    let second = (round && round.second !== null) ? round.second : ' ';
+    let third = '';
     const total = (round && round.total !== null) ? round.total : ' ';
+
+    if (props.bonus && roundIndex === 9 && props.scores.length > 10) second = props.scores[10].first;
+    if (props.bonus && roundIndex === 9 && props.scores.length === 12) third = <div className="frame">{props.scores[11].first}</div>;
+
     return (
       <div className="score-container" key={`round-${roundIndex}`}>
         <div className="frame-container">
           <div className="frame">{first}</div>
           <div className="frame">{second}</div>
+          {third}
         </div>
         <div className="total-score">{total}</div>
       </div>
