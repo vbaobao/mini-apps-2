facaf37c1b55d35e5feedb1df0fecad90103952a
vbaobao <vbao.main@gmail.com>
2021-06-23 19:14:33 -0700

    add board reset and playing functionality

--- challenge_4/my-app/src/App.js
+++ challenge_4/my-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { connect } from 'react-redux';
 import { mapStateToProps, mapDispatchToProps } from './mapToProps';
+import Board from './components/Board';
 
 function App(props) {
   const boardSize = 10;
   const numberOfMines = 10;
   const reset = () => { props.resetGame(boardSize, numberOfMines); };
-  const sweep = (e) => { props.sweep(Number(e.target.value), props.game.board, props.game.revealed, boardSize); };
+  const sweep = (index) => { props.sweep(Number(index), props.game.board, props.game.revealed, boardSize); };
 
   return (
     <div className="App">
@@ -15,6 +16,13 @@ function App(props) {
       </pre>
       <button onClick={reset}>Test reset</button>
       <button onClick={sweep} value="3">Test sweep</button>
+      <Board
+        board={props.game.board}
+        revealed={props.game.revealed}
+        status={props.game.status}
+        boardSize={boardSize}
+        sweep={sweep}
+      />
     </div>
   );
 }
