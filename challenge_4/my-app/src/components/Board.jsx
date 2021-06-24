3e6bb4a46611162f052675691fe116a396342404
vbaobao <vbao.main@gmail.com>
2021-06-23 19:14:59 -0700

    create board component for game visuals

---
+++ challenge_4/my-app/src/components/Board.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Board (props) {
+  console.log(props);
+  const clickHandler = (e) => {
+    props.sweep(e.target.getAttribute('value'));
+  };
+
+  const board = props.board.map((cell, index) => {
+    const isRevealed = props.revealed[index];
+    return (
+      <div
+        key={index} value={index}
+        className={isRevealed ? 'cell-container' : 'cell-container hidden'}
+        onClick={clickHandler}
+      >
+        <span>
+          {isRevealed ? cell : ' '}
+        </span>
+      </div>
+    );
+  });
+
+  return (
+    <div className="board-container">
+      {board}
+    </div>
+  );
+}
+
+export default Board;
